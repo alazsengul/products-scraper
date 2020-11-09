129ae6de4c9fb76dcfc9adfa0dbe9d6286b3d31f
Alaz Sengul <alazsengul@gmail.com>
2020-11-09 00:11:55 -0500

    added amazon url page iteration

--- src/scraping.py
+++ src/scraping.py
@@ -2,18 +2,7 @@ from selectorlib import Extractor
 from selectorlib.formatter import Formatter
 import requests
 import json
-from time import sleep
-
-# ------------------------------------------------------------------------------
-# Amazon Image Formatter
-# -> returns highest quality image from Amazon's various sizes
-#
-# text: (STRING) comma-separated image urls
-#
-
-class AmazonImage(Formatter):
-    def format(self, text):
-        return text.split(", ")[-1].split()[0]
+import time
 
 # ------------------------------------------------------------------------------
 # REQUEST URL
@@ -33,24 +22,7 @@ def request_url(url):
         "Upgrade-Insecure-Requests": "1"
     }
 
-    # headers = {
-    #     'dnt': '1',
-    #     'upgrade-insecure-requests': '1',
-    #     'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-    #     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    #     'sec-fetch-site': 'same-origin',
-    #     'sec-fetch-mode': 'navigate',
-    #     'sec-fetch-user': '?1',
-    #     'sec-fetch-dest': 'document',
-    #     'referer': 'https://www.amazon.com/',
-    #     'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    # }
-
     request = requests.get(url, headers = headers)
-
-    # if request.status_code > 500:
-    #     print(f"Request to page {url} failed.")
-    #     return None
 
     return request
 
@@ -66,25 +38,67 @@ def extract_data(extractor, request):
     return extractor.extract(request.text)
 
 # ------------------------------------------------------------------------------
+# Amazon Image Formatter
+# -> returns highest quality image from Amazon's various sizes
+#
+# text: (STRING) comma-separated image urls
+#
+
+class AmazonImage(Formatter):
+    def format(self, text):
+        return text.split(", ")[-1].split()[0]
+
+# ------------------------------------------------------------------------------
+# CONSTRUCT AMAZON URL
+# -> returns constructed Amazon URL
+#
+# search_query: (STRING) user's raw search query
+# page_number: (INT) search result page number
+# time_stamp: (INT) current Unix time stamp
+# 
+
+def construct_amazon_url(search_query, page_number, time_stamp=int(time.time())):
+    
+    base_url = "https://www.amazon.com/s?"
+    query_param = "k=" + "+".join(search_query.split())
+    page_param = "page=" + str(page_number)
+    dt_param = "qid=" + str(time_stamp)
+    
+    if page_number == 1:
+        ref_param = "ref=nb_sb_noss"
+        return base_url + "&".join([query_param, dt_param, ref_param])
+    else:
+        ref_param = "ref=sr_pg_" + str(page_number)
+        return base_url + "&".join([query_param, page_param, dt_param, ref_param])
+
+# ------------------------------------------------------------------------------
 # SCRAPE AMAZON
 # -> returns Amazon search results
 #
-# search_query: (STRING) user's cleaned search query
+# search_query: (STRING) user's raw search query
+# page_last: (INT) last product page number in range to scrape
+# page_first: (INT) first product page number in range to scrape
+# time_delay: (INT) amount of time to pause for before each page request
 #
 
-def scrape_amazon(search_query):
+def scrape_amazon(search_query, page_last, page_first=1, time_delay=2):
 
-    url_query = "https://www.amazon.com/s?k=weighted+blanket&ref=nb_sb_noss"
-
-    amazon_request = request_url(url_query)
     formatters = Formatter.get_all()
     amazon_extractor = Extractor.from_yaml_file("selectors/amazon.yml", formatters=formatters)
-    scraped_data = extract_data(amazon_extractor, amazon_request)
 
-    print(scraped_data)
+    result = []
 
-    return scraped_data
+    for page_index in range(page_first, page_last + 1):
+
+        url_query = construct_amazon_url("weighted blanket", page_index)
+        amazon_request = request_url(url_query)
+        scraped_data = extract_data(amazon_extractor, amazon_request)
+
+        result += scraped_data["products"]
+
+    return result
 
 if __name__ == '__main__':
-    search_query = "weighted blanket"
-    scrape_amazon(search_query)
+    result = scrape_amazon("weighted blanket", 3)
+    print(len(result))
+    print(result)
